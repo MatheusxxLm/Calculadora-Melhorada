a8620168a07cac6f48eaab3ebb03caa17f4f3092
Matheus Martins01 <97361856+MatheusxxLm@users.noreply.github.com>
2022-07-05 19:57:43 -0300

    Update Calculadora.cs

--- Calculadora.cs
+++ Calculadora.cs
@@ -15,95 +15,53 @@ namespace ConsoleApp2
         }
         static void menu()
         {
-            Console.WriteLine("qual tipo de calculo você deseja fazer?");
-            Console.WriteLine("1 - soma");
-            Console.WriteLine("2 - divisão");
-            Console.WriteLine("3 - multiplicação");
-            Console.WriteLine("4 - subtração");
-            Console.WriteLine("5 - sair");
-            Console.WriteLine("-------------------------------------");
-            Console.WriteLine("selecione a opção");
-            short op = short.Parse(Console.ReadLine());
+        inicio:
+        Console.WriteLine();
+        Console.WriteLine("qual tipo de calculo você deseja fazer?");
+        Console.WriteLine("1 - soma");
+        Console.WriteLine("2 - divisão");
+        Console.WriteLine("3 - multiplicação");
+        Console.WriteLine("4 - subtração");
+        
+        short op = short.Parse(Console.ReadLine());
 
-            if (op == 1)
-            {
-                soma();
-            }
-            else if (op == 2)
-            {
-                divisao();
-            }
-            else if (op == 3)
-            {
-                multiplicacao();
-            }
-            else if (op == 4)
-            {
-                subtracao();
-            }
-            else if (op == 5)
-            {
-                System.Environment.Exit(0);
-            }
-            else if (op >= 6)
-            {
-                
-                Console.WriteLine("opção inválida.");
-                Console.WriteLine("////////////////////////////////////");
-            }
-            menu();
-            Console.ReadLine();
+        if (op != 1 && op != 2 && op != 3 && op != 4) { Console.WriteLine("Opção inválida."); goto inicio; } 
+                 
+        Console.WriteLine("primeiro valor: "); 
+        double num1 = double.Parse(Console.ReadLine());
+        
+        Console.WriteLine("segundo valor: ");  
+        double num2 = double.Parse(Console.ReadLine());
+            
+        switch (op)
+        {
+            case 1:
+                soma(num1, num2);
+                break;
+            case 2: 
+                divisao(num1, num2);
+                break;
+            case 3:
+                subtracao(num1, num2);
+                break;
+            case 4:
+                multiplicacao(num1, num2);
+                break;           
+        }
+        
+        Console.ReadLine();   
             
         }
-        static void soma()
-        {
-
-            Console.WriteLine("primeiro valor: ");
-            int num1 = int.Parse(Console.ReadLine());
-            Console.WriteLine("segundo valor: ");
-            int num2 = int.Parse(Console.ReadLine());
-            int resultado = num1 + num2;
-            Console.WriteLine("o resultado é ");
-            Console.WriteLine($"{num1 + num2}");
-            Console.ReadLine();
-
-        }
-        static void divisao()
-        {
-            Console.WriteLine("primeiro valor: ");
-            int num1 = int.Parse(Console.ReadLine());
-            Console.WriteLine("segundo valor: ");
-            int num2 = int.Parse(Console.ReadLine());
-            int resultado = num1 / num2;
-            Console.WriteLine("o resultado é ");
-            Console.WriteLine($"{num1 / num2}");
-            Console.ReadLine();
-        }
-        static void subtracao()
-        {
-            Console.WriteLine("primeiro valor: ");
-            int num1 = int.Parse(Console.ReadLine());
-            Console.WriteLine("segundo valor: ");
-            int num2 = int.Parse(Console.ReadLine());
-            int resultado = num1 - num2;
-            Console.WriteLine("o resultado é ");
-            Console.WriteLine($"{num1 - num2}");
-            Console.ReadLine();
-        }
-        
-        static void multiplicacao()
-        {
-            Console.WriteLine("primeiro valor: ");
-            int num1 = int.Parse(Console.ReadLine());
-            Console.WriteLine("segundo valor: ");
-            int num2 = int.Parse(Console.ReadLine());
-            int resultado = num1 * num2;
-            Console.WriteLine("o resultado é ");
-            Console.WriteLine($"{num1 * num2}");
-            Console.ReadLine();
-        }
 
         
-
+        static void soma(double n1, double n2) => Console.WriteLine(n1 + n2);
+    
+        static void divisao(double n1, double n2) => Console.WriteLine(n1 / n2);
+    
+        static void subtracao(double n1, double n2) => Console.WriteLine(n1 - n2);
+    
+        static void multiplicacao(double n1, double n2) => Console.WriteLine(n1 * n2);
+        
+        
     }
 }
